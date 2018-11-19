d18f05131f3adb263ec6b336b6626c9c56dad50c
dongrami <dongku93@naver.com>
2018-11-19 16:15:11 +0900

    2018-11-19

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function aboutOver() {
     $(".gear3").addClass("hat_ani");
     $(".hat-cont").show();
     $(".about").stop().animate({"top":"-75%"}, 300);
-    $(".content01").stop().slideDown(1000);
+    $(".content01").stop().slideDown(500);
 }
 
 function aboutBack() {
@@ -23,7 +23,7 @@ function aboutBack() {
     $(".about").stop().animate({"top":"-20%"}, 300);
 }
 $(".about").mouseover(aboutOver);
-$(".exit").click(aboutBack);
+$(".exit-A").click(aboutBack);
 
 
 /**** My design ****/
@@ -35,6 +35,7 @@ function designOver() {
         $(".ear-gear1").stop().animate({"left": "0%"}, 500);
         $(".ear-gear2").stop().animate({"right": "0%"}, 600);
         $(".design").stop().animate({"left": "-98%"}, 300);
+        $(".content02").stop().slideDown(500);
     }
 function designBack() {
         $(".ear-left").stop().animate({"left": "-13%"}, 300);
@@ -44,20 +45,23 @@ function designBack() {
         $(".ear-gear1").stop().animate({"left": "100%"}, 500);
         $(".ear-gear2").stop().animate({"right": "100%"}, 500);
         $(".design").stop().animate({"left": "-48%"}, 300);
+        $(".content02").hide();
 
     };
     $(".design").mouseover(designOver);
-    $(".ear").mouseleave(designBack);
+    $(".exit-D").click(designBack);
 
     function webOver() {
     $(".arm_bot02").stop().removeClass("arm_ani_back").addClass("arm_ani");
     $(".elbow-gear").stop().removeClass("elbow_ani_back").addClass("elbow_ani");
-    $(".web").stop().animate({"top": "-20%"}, 500);
+    $(".web").stop().animate({"top": "-30%","right": "-80%"}, 500);
+    $(".content03").stop().slideDown(500);
 };
 function webBack() { 
     $(".arm_bot02").stop().removeClass("arm_ani").addClass("arm_ani_back");
     $(".elbow-gear").stop().removeClass("elbow_ani").addClass("elbow_ani_back");
-    $(".web").stop().animate({"top": "-10%"}, 500);
+    $(".web").stop().animate({"top": "-15%","right": "-50%"}, 500);
+    $(".content03").hide();
 };
 $(".web").mouseover(webOver);
-    $(".arm_bot02, .arm02").mouseleave(webBack);
+$(".exit-W").click(webBack);
