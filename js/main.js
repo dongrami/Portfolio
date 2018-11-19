89e4d75654de4027e9c1042140069aa318267b5a
dongrami <dongku93@naver.com>
2018-11-19 11:25:43 +0900

    2018-11-19

--- js/main.js
+++ js/main.js
@@ -1,39 +1,19 @@
 /**** About me ****/
 function aboutOver() {
-    $(".hat").stop().animate({
-        "bottom": "160%"
-    }, 500);
-    $(".hat-cont .gear1").stop().animate({
-        "top": "0"
-    });
-    $(".hat-cont .gear2").stop().animate({
-        "top": "-98%"
-    }, 500);
-    $(".hat-cont .gear3").stop().animate({
-        "bottom": "55%"
-    }, 800);
-    $(".hat-cont .gear4").stop().animate({
-        "top": "-70%"
-    }, 800);
+    $(".hat").stop().animate({"bottom": "160%"}, 300);
+    $(".hat-cont .gear1").stop().animate({"top": "0"});
+    $(".hat-cont .gear2").stop().animate({"top": "-98%"}, 500);
+    $(".hat-cont .gear3").stop().animate({"bottom": "55%"}, 800);
+    $(".hat-cont .gear4").stop().animate({"top": "-70%"}, 800);
     $(".hat-cont").show();
 }
 
 function aboutBack() {
-    $(".hat").stop().animate({
-        "bottom": "100%"
-    }, 500);
-    $(".hat-cont .gear1").stop().animate({
-        "top": "2%"
-    });
-    $(".hat-cont .gear2").stop().animate({
-        "top": "2%"
-    }, 500);
-    $(".hat-cont .gear3").stop().animate({
-        "bottom": "1%"
-    }, 500);
-    $(".hat-cont .gear4").stop().animate({
-        "top": "5%"
-    }, 500);
+    $(".hat").stop().animate({"bottom": "100%"}, 300);
+    $(".hat-cont .gear1").stop().animate({"top": "2%"});
+    $(".hat-cont .gear2").stop().animate({"top": "2%"}, 500);
+    $(".hat-cont .gear3").stop().animate({"bottom": "1%"}, 500);
+    $(".hat-cont .gear4").stop().animate({"top": "5%"}, 500);
     $(".hat-cont").hide();
 }
 $(".about").mouseover(aboutOver);
@@ -41,35 +21,24 @@ $(".hat").mouseleave(aboutBack);
 
 
 /**** My design ****/
-$(".design").hover(function () {
-        $(".ear-left").stop().animate({
-            "left": "-63%"
-        }, 500);
-        $(".ear-1-left").stop().animate({
-            "left": "-70%"
-        }, 500);
-        $(".ear-right").stop().animate({
-            "right": "-63%"
-        }, 500);
-        $(".ear-1-right").stop().animate({
-            "right": "-70%"
-        }, 500);
-    },
-    function () {
-        $(".ear-left").stop().animate({
-            "left": "-13%"
-        }, 500);
-        $(".ear-1-left").stop().animate({
-            "left": "-15%"
-        }, 500);
-        $(".ear-right").stop().animate({
-            "right": "-13%"
-        }, 500);
-        $(".ear-1-right").stop().animate({
-            "right": "-15%"
-        }, 500);
-    });
-
+function designOver() {
+        $(".ear-left").stop().animate({"left": "-63%"}, 300);
+        $(".ear-1-left").stop().animate({"left": "-70%"}, 300);
+        $(".ear-right").stop().animate({"right": "-63%"}, 300);
+        $(".ear-1-right").stop().animate({"right": "-70%"}, 300);
+        $(".ear-gear1").stop().animate({"left": "0%"}, 500);
+        $(".ear-gear2").stop().animate({"right": "0%"}, 500);
+    }
+function designBack() {
+        $(".ear-left").stop().animate({"left": "-13%"}, 300);
+        $(".ear-1-left").stop().animate({"left": "-15%"}, 300);
+        $(".ear-right").stop().animate({"right": "-13%"}, 300);
+        $(".ear-1-right").stop().animate({"right": "-15%"}, 300);
+        $(".ear-gear1").stop().animate({"left": "100%"}, 500);
+        $(".ear-gear2").stop().animate({"right": "100%"}, 500);
+    };
+    $(".design").mouseover(designOver);
+    $(".ear").mouseleave(designBack);
 
 $(".web").hover(function () {
     $(".arm_bot02").stop().removeClass("arm_ani_back").addClass("arm_ani");
