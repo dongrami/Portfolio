ba5d755084dc200dc026938bc51d20a9b93e7e52
dongrami <dongku93@naver.com>
2018-11-20 17:49:54 +0900

    2018-11-20

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ function designBack() {
     };
     $(".design").mouseover(designOver);
     $(".exit-D").click(designBack);
-
+/*******web site****/
     function webOver() {
     $(".arm_bot02").stop().removeClass("arm_ani_back").addClass("arm_ani");
     $(".elbow-gear").stop().removeClass("elbow_ani_back").addClass("elbow_ani");
@@ -65,3 +65,98 @@ function webBack() {
 };
 $(".web").mouseover(webOver);
 $(".exit-W").click(webBack);
+
+/*** color ***/
+    function bluecolor (){
+        $(".bg").addClass("B-bg-color").removeClass("Y-bg-color");
+        $(".hand-blank").addClass("B-bg-color").removeClass("Y-bg-color");
+        $(".head").addClass("B-main-color").removeClass("Y-main-color");
+        $(".b1").addClass("B-main-color").removeClass("Y-main-color");
+        $(".arm01").addClass("B-main-color").removeClass("Y-main-color");
+        $(".arm02").addClass("B-main-color").removeClass("Y-main-color");
+        $(".arm_bot01").addClass("B-main-color").removeClass("Y-main-color");
+        $(".arm_bot02").addClass("B-main-color").removeClass("Y-main-color");
+        $(".hat").addClass("B-sub-color").removeClass("Y-sub-color");
+        $(".ear-left").addClass("B-sub-color").removeClass("Y-sub-color");
+        $(".ear-right").addClass("B-sub-color").removeClass("Y-sub-color");
+        $(".hand-neck01").addClass("B-sub-color").removeClass("Y-sub-color");
+        $(".hand-neck02").addClass("B-sub-color").removeClass("Y-sub-color");
+        $(".link01").addClass("B-sub-color").removeClass("Y-sub-color");
+        $(".link02").addClass("B-sub-color").removeClass("Y-sub-color");
+        $(".neck").addClass("B-sub-color").removeClass("Y-sub-color");
+        $(".ear-1-left").addClass("B-line-color").removeClass("Y-line-color");
+        $(".ear-1-right").addClass("B-line-color").removeClass("Y-line-color");
+        $(".mouse").addClass("B-line-color").removeClass("Y-line-color");
+        $(".chest").addClass("B-line-color").removeClass("Y-chest-color");
+        $(".part1").addClass("B-part-color").removeClass("Y-part-color");
+        $(".part2").addClass("B-part-color").removeClass("Y-part-color");
+        $(".part3").addClass("B-part-color").removeClass("Y-part-color");
+        $(".part2-1").addClass("B-part-color-a").removeClass("Y-part-color-a");
+        $(".part3-1").addClass("B-part-color-b").removeClass("Y-part-color-b");
+        $(".eye-1").addClass("B-eye-color").removeClass("Y-eye-color");
+        $(".eye-2").addClass("B-eye-color").removeClass("Y-eye-color");
+    }
+    $(".p-blue").click(bluecolor);
+    
+
+    function redcolor (){
+        $(".bg").removeClass("B-bg-color").removeClass("Y-bg-color");
+        $(".hand-blank").removeClass("B-bg-color").removeClass("Y-bg-color");
+        $(".head").removeClass("B-main-color").removeClass("Y-main-color");
+        $(".b1").removeClass("B-main-color").removeClass("Y-main-color");
+        $(".arm01").removeClass("B-main-color").removeClass("Y-main-color");
+        $(".arm02").removeClass("B-main-color").removeClass("Y-main-color");
+        $(".arm_bot01").removeClass("B-main-color").removeClass("Y-main-color");
+        $(".arm_bot02").removeClass("B-main-color").removeClass("Y-main-color");
+        $(".hat").removeClass("B-sub-color").removeClass("Y-sub-color");
+        $(".ear-left").removeClass("B-sub-color").removeClass("Y-sub-color");
+        $(".ear-right").removeClass("B-sub-color").removeClass("Y-sub-color");
+        $(".hand-neck01").removeClass("B-sub-color").removeClass("Y-sub-color");
+        $(".hand-neck02").removeClass("B-sub-color").removeClass("Y-sub-color");
+        $(".link01").removeClass("B-sub-color").removeClass("Y-sub-color");
+        $(".link02").removeClass("B-sub-color").removeClass("Y-sub-color");
+        $(".neck").removeClass("B-sub-color").removeClass("Y-sub-color");
+        $(".ear-1-left").removeClass("B-line-color").removeClass("Y-line-color");
+        $(".ear-1-right").removeClass("B-line-color").removeClass("Y-line-color");
+        $(".mouse").removeClass("B-line-color").removeClass("Y-line-color");
+        $(".chest").removeClass("B-line-color").removeClass("Y-chest-color");
+        $(".part1").removeClass("B-part-color").removeClass("Y-part-color");
+        $(".part2").removeClass("B-part-color").removeClass("Y-part-color");
+        $(".part3").removeClass("B-part-color").removeClass("Y-part-color");
+        $(".part2-1").removeClass("B-part-color-a").removeClass("Y-part-color-a");
+        $(".part3-1").removeClass("B-part-color-b").removeClass("Y-part-color-b");
+        $(".eye-1").removeClass("B-eye-color").removeClass("Y-eye-color");
+        $(".eye-2").removeClass("B-eye-color").removeClass("Y-eye-color");
+    }
+    $(".p-red").click(redcolor);
+
+    function yellowcolor (){
+        $(".bg").removeClass("B-bg-color").addClass("Y-bg-color");
+        $(".hand-blank").removeClass("B-bg-color").addClass("Y-bg-color");
+        $(".head").removeClass("B-main-color").addClass("Y-main-color");
+        $(".b1").removeClass("B-main-color").addClass("Y-main-color");
+        $(".arm01").removeClass("B-main-color").addClass("Y-main-color");
+        $(".arm02").removeClass("B-main-color").addClass("Y-main-color");
+        $(".arm_bot01").removeClass("B-main-color").addClass("Y-main-color");
+        $(".arm_bot02").removeClass("B-main-color").addClass("Y-main-color");
+        $(".hat").removeClass("B-sub-color").addClass("Y-sub-color");
+        $(".ear-left").removeClass("B-sub-color").addClass("Y-sub-color");
+        $(".ear-right").removeClass("B-sub-color").addClass("Y-sub-color");
+        $(".hand-neck01").removeClass("B-sub-color").addClass("Y-sub-color");
+        $(".hand-neck02").removeClass("B-sub-color").addClass("Y-sub-color");
+        $(".link01").removeClass("B-sub-color").addClass("Y-sub-color");
+        $(".link02").removeClass("B-sub-color").addClass("Y-sub-color");
+        $(".neck").removeClass("B-sub-color").addClass("Y-sub-color");
+        $(".ear-1-left").removeClass("B-line-color").addClass("Y-line-color");
+        $(".ear-1-right").removeClass("B-line-color").addClass("Y-line-color");
+        $(".mouse").removeClass("B-line-color").addClass("Y-line-color");
+        $(".chest").removeClass("B-line-color").addClass("Y-chest-color");
+        $(".part1").removeClass("B-part-color").addClass("Y-part-color");
+        $(".part2").removeClass("B-part-color").addClass("Y-part-color");
+        $(".part3").removeClass("B-part-color").addClass("Y-part-color");
+        $(".part2-1").removeClass("B-part-color-a").addClass("Y-part-color-a");
+        $(".part3-1").removeClass("B-part-color-b").addClass("Y-part-color-b");
+        $(".eye-1").removeClass("B-eye-color").addClass("Y-eye-color");
+        $(".eye-2").removeClass("B-eye-color").addClass("Y-eye-color");
+    }
+    $(".p-yellow").click(yellowcolor);
