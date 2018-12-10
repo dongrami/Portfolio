51b51413f3c7bef66b4fa573d78ab8c1c1954c34
dongrami <dongku93@naver.com>
2018-12-10 10:29:56 +0900

    20181210

--- public/js/web.js
+++ public/js/web.js
@@ -17,3 +17,23 @@ function(){
     $(".text .txt").hide();
 
 });
+
+$(".site03").hover(function(){   
+    $(".site03 .text").stop().animate({"right":"0%"}, 500);
+    $(".text .txt").show();
+},
+function(){
+    $(".site03 .text").stop().animate({"right":"100%"}, 300);
+    $(".text .txt").hide();
+
+});
+
+$(".site04").hover(function(){   
+    $(".site04 .text").stop().animate({"right":"0%"}, 500);
+    $(".text .txt").show();
+},
+function(){
+    $(".site04 .text").stop().animate({"right":"100%"}, 300);
+    $(".text .txt").hide();
+
+});
