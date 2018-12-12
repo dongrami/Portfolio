128f83050ac4146413f68677695dc46d45a4a80d
dongrami <dongku93@naver.com>
2018-12-12 18:51:32 +0900

    20181212

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -2,3 +2,17 @@ $(".navs_mo > ul").slideUp();
 $(".navs_mo > i").click(function(){
 	$(".navs_mo > ul").slideToggle(100);
 });
+function ab (){
+$(".wrap").hide();
+$(".about-me").show();
+$("body").css({"background-color":"#fff"});
+};
+$(".about").click(ab);
+
+function Close (){
+$(".about-me").hide();
+$(".wrap").show();
+$("body").css({"background-color":"#8ce0cd"});
+
+};
+$(".btn").click(Close);
--- public/js/web.js
+++ public/js/web.js
@@ -37,3 +37,59 @@ function(){
     $(".text .txt").hide();
 
 });
+
+/***************날씨 *****************/
+
+$("#modal_open").click(function () {
+	$("#modal").show()
+});
+$("#modal_close").click(function () {
+	$("#modal").hide()
+});
+$.ajax({
+	url: "../json/city.json",
+	type: "get",
+	dataType: "json",
+	success: function (data) {
+		var city = data.cities;
+		var html = '';
+		for (var i = 0; i < city.length; i++) {
+			html = '<option value="' + city[i].id + '">' + city[i].name + '</option>';
+			$("#area").append(html);
+        }
+        $("#area").trigger("change");
+	},
+	error: function (xhr, status, error) {
+		console.log(xhr, status, error);
+	}
+});
+$("#area").change(function () {
+	var id = $(this).val();
+	var city = $(this).find('option:selected').text();
+	var appid = "02efdd64bdc14b279bc91d9247db4722";
+	var units = "metric";
+	var dt = new Date();
+	var date = dt.getFullYear() + ". " + (dt.getMonth() + 1) + ". " + dt.getDate();
+	$.ajax({
+		url: "https://api.openweathermap.org/data/2.5/weather",
+		type: "get",
+		dataType: "json",
+		data: {
+			id: id,
+			appid: appid,
+			units: units
+		},
+		success: function (data) {
+			$(".dl_icon").attr("src", "../images/web/" + data.weather[0].icon + ".png");
+			$(".dl_area > p").html(city);
+			$(".dl_date").html(date);
+			$(".dl_temp").html(data.main.temp + '℃');
+			$(".dl_temp2").html('최고:' + data.main.temp_max + '℃ / 최저:' + data.main.temp_min + '℃');
+			$(".dl_desc").html(data.weather[0].description);
+			$("#modal").hide();
+		},
+		error: function (xhr, status, error) {
+			console.log(xhr, status, error);
+		}
+    });
+});
